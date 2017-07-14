0acf67ec9884037936b49962949fb441b1604b3b
Mahesh Chandra <mmahesh.chandra873@gmail.com>
2017-07-14 18:08:18 +0200

    new README

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# variants_of_rmsprop_and_adagrad
-SC-Adagrad, SC-RMSProp and RMSProp variants proposed in  https://arxiv.org/abs/1706.05507
+# Variants of Adagrad and RMSProp
 
-# Contributors
+Keras implementation of SC-Adagrad, SC-RMSProp and RMSProp Algorithms proposed in [here](https://arxiv.org/abs/1706.05507)
 
-Authors of Keras and me
+Short version accepted at ICML, 17 can be found [here](http://www.ml.uni-saarland.de/Publications/MukHei-VariantsRMSPropAdagradLogRegret.pdf)
+
+I wrote a blog [here](https://mmahesh.github.io/articles/2017-07/tutorial-on-sc-adagrad-a-new-stochastic-gradient-method-for-deep-learning) describing the algorithms in simple terms so that it is easy to understand the gist of the algorithms.
+
+
+# Usage
+
+So, you created a network, now you want to train with above algorithms. Copy the file into new_optimizers.py into your repository
+
+```python
+from new_optimizers.py import *
+
+# lets for example you want to use SC-Adagrad then
+# create optimizer object as follows.
+
+sc_adagrad = SC_Adagrad()
+```
+
+Then in the code where you compile your model you must set 'optimizer=sc_adagrad'. You can do the same for SC-RMSProp and RMSProp algorithms.
+
+## I hope this is helpful :).
--- new_optimizers.py
+++ new_optimizers.py
@@ -11,7 +11,7 @@ I appreciate all the authors who contributed to keras.
 """
 
 from keras import backend as K
-
+from keras.optimizers import *
 
 class SC_Adagrad(Optimizer):
     """SC-Adagrad optimizer.
@@ -22,7 +22,6 @@ class SC_Adagrad(Optimizer):
         lr: float >= 0. Learning rate.
         xi_1: float, 0 < xi_1 < 1. Generally close to 1.
         xi_2: float, 0 < xi_2 < 1. Generally close to 1.
-        epsilon: float >= 0. Fuzz factor.
         decay: float >= 0. Learning rate decay over each update.
 
     # References
@@ -48,7 +47,7 @@ class SC_Adagrad(Optimizer):
             lr *= (1. / (1. + self.decay * self.iterations))
 
 
-        vs = [K.zeros(K.get_variable_shape(p), dtype=K.dtype(p)) for p in params]
+        vs = [K.zeros(K.get_variable_shape(p)) for p in params]
         self.weights = [self.iterations]+ vs
 
         for p, g, v in zip(params, grads, vs):
@@ -71,4 +70,130 @@ class SC_Adagrad(Optimizer):
                   'xi_2': float(K.get_value(self.xi_2)),
                   'decay': float(K.get_value(self.decay))}
         base_config = super(SC_Adagrad, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+
+class SC_RMSProp(Optimizer):
+    """SC-RMSProp optimizer.
+
+    Default parameters follow those provided in the original paper.
+
+    # Arguments
+        lr: float >= 0. Learning rate.
+        xi_1: float, 0 < xi_1 . Fuzz factor params. 
+        xi_2: float, 0 < xi_2 . Fuzz factor params.
+        gamma: float >= 0. Weighing factor.
+        decay: float >= 0. Learning rate decay over each update.
+
+    # References
+        - [Variants of RMSProp and Adagrad with Logarithmic Regret Bounds](http://arxiv.org/abs/1706.05507)
+    """
+
+    def __init__(self, lr=0.01, xi_1=0.1, xi_2=0.1,
+                 gamma=0.9, decay=0., **kwargs):
+        super(SC_RMSProp, self).__init__(**kwargs)
+        self.iterations = K.variable(0, name='iterations')
+        self.lr = K.variable(lr, name='lr')
+        self.xi_1 = K.variable(xi_1, name='xi_1')
+        self.xi_2 = K.variable(xi_2, name='xi_2')
+        self.gamma = K.variable(gamma, name='gamma')
+        self.decay = K.variable(decay, name='decay')
+        self.initial_decay = decay
+
+    def get_updates(self, params, constraints, loss):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+        if self.initial_decay > 0:
+            lr *= (1. / (1. + self.decay * self.iterations))
+
+        t = self.iterations + 1
+
+        vs = [K.zeros(K.get_variable_shape(p)) for p in params]
+        self.weights = [self.iterations]+ vs
+
+        for p, g, v in zip(params, grads, vs):
+            v_t = (1-(self.gamma/t))*v + (self.gamma/t)*K.square(g)
+            p_t = p - self.lr * g / (t*v_t + self.xi_2*K.exp(-self.xi_1*t*v_t) )
+
+            self.updates.append(K.update(v, v_t))
+
+            new_p = p_t
+            # apply constraints
+            if p in constraints:
+                c = constraints[p]
+                new_p = c(new_p)
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {'lr': float(K.get_value(self.lr)),
+                  'xi_1': float(K.get_value(self.xi_1)),
+                  'xi_2': float(K.get_value(self.xi_2)),
+                  'gamma': float(K.get_value(self.gamma)),
+                  'decay': float(K.get_value(self.decay))}
+        base_config = super(SC_RMSProp, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class RMSProp_variant(Optimizer):
+    """RMSProp optimizer.
+
+    Default parameters follow those provided in the original paper.
+
+    # Arguments
+        lr: float >= 0. Learning rate.
+        gamma: 0 < gamma <= 1, Weighing factor.
+        delta: float >= 0. Fuzz factor.
+        decay: float >= 0. Learning rate decay over each update.
+
+    # References
+        - [Variants of RMSProp and Adagrad with Logarithmic Regret Bounds](http://arxiv.org/abs/1706.05507)
+    """
+
+    def __init__(self, lr=0.01, delta=1e-8, 
+                 gamma=0.9, decay=0., **kwargs):
+        super(RMSProp_variant, self).__init__(**kwargs)
+        self.iterations = K.variable(0, name='iterations')
+        self.lr = K.variable(lr, name='lr')
+        self.delta = K.variable(delta, name='delta')
+        self.gamma = K.variable(gamma, name='gamma')
+        self.decay = K.variable(decay, name='decay')
+        self.initial_decay = decay
+
+    def get_updates(self, params, constraints, loss):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+        if self.initial_decay > 0:
+            lr *= (1. / (1. + self.decay * self.iterations))
+
+        t = self.iterations + 1
+
+        vs = [K.zeros(K.get_variable_shape(p)) for p in params]
+        self.weights = [self.iterations]+ vs
+
+        for p, g, v in zip(params, grads, vs):
+            v_t = (1-(self.gamma/t))*v + (self.gamma/t)*K.square(g)
+            p_t = p - self.lr * g / (K.sqrt(t*v_t) + self.delta )
+
+            self.updates.append(K.update(v, v_t))
+
+            new_p = p_t
+            # apply constraints
+            if p in constraints:
+                c = constraints[p]
+                new_p = c(new_p)
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {'lr': float(K.get_value(self.lr)),
+                  'delta': float(K.get_value(self.delta)),
+                  'gamma': float(K.get_value(self.gamma)),
+                  'decay': float(K.get_value(self.decay))}
+        base_config = super(RMSProp_variant, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
